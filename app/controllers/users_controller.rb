c9e366cdc64411dfd9e09b62297bd3da7011d039
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-03-28 16:17:37 -0300

    add feature to create standard measurements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,13 @@ class UsersController < ApplicationController
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
+    Measurement.create(name:"Left Arm",created_by:user.id)
+    Measurement.create(name:"Right Arm",created_by:user.id)
+    Measurement.create(name:"Left Leg",created_by:user.id)
+    Measurement.create(name:"Right Leg",created_by:user.id)
+    Measurement.create(name:"Waist",created_by:user.id)
+    Measurement.create(name:"Abdomen",created_by:user.id)
+
     json_response(response, :created)
   end
 
