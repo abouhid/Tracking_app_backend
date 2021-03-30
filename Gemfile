9f5494dfd083ef66acd9b5de94643c5df0bedeaa
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-03-30 14:57:44 -0300

    rubocop applied

--- Gemfile
+++ Gemfile
@@ -33,14 +33,12 @@ group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails', '~> 5.0.0'
   gem 'sqlite3', '~> 1.4'
-
 end
 group :test do
   gem 'database_cleaner'
   gem 'factory_bot_rails', '~> 4.0'
   gem 'shoulda-matchers', '~> 3.1'
   gem 'sqlite3', '~> 1.4'
-
 end
 group :development do
   gem 'listen', '~> 3.3'
@@ -49,9 +47,8 @@ group :development do
   gem 'spring'
 end
 
-group :production do 
-gem 'pg'
-
+group :production do
+  gem 'pg'
 end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ class UsersController < ApplicationController
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
-    Measurement.create(name:"Left Arm",created_by:user.id)
-    Measurement.create(name:"Right Arm",created_by:user.id)
-    Measurement.create(name:"Left Leg",created_by:user.id)
-    Measurement.create(name:"Right Leg",created_by:user.id)
-    Measurement.create(name:"Waist",created_by:user.id)
-    Measurement.create(name:"Abdomen",created_by:user.id)
+    Measurement.create(name: 'Left bicep', created_by: user.id)
+    Measurement.create(name: 'Right bicep', created_by: user.id)
+    Measurement.create(name: 'Left Thigh', created_by: user.id)
+    Measurement.create(name: 'Right Thigh', created_by: user.id)
+    Measurement.create(name: 'Waist', created_by: user.id)
+    Measurement.create(name: 'Hips', created_by: user.id)
 
     json_response(response, :created)
   end
