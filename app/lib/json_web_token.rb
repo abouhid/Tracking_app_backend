27f1e1203a8e876f43daccbfabbe133e1af4826c
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-04-03 16:27:49 -0300

    change jwt

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,6 +1,9 @@
 class JsonWebToken
-  # secret to encode and decode token
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  HMAC_SECRET = if Rails.env.production?
+                  ENV['SECRET_KEY_BASE']
+                else
+                  Rails.application.secrets.secret_key_base
+                end
 
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
